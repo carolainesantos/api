4b28a3add3b8593e99b64f6941cffc6504830c07
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-06-15 19:14:08 -0300

    chore: add AppContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import AppRoutes from "./routes/AppRoutes";
+import { AppContainer } from "./styles";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <AppContainer>
+      <AppRoutes />
+    </AppContainer>
+  );
 }
